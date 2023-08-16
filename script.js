51263c2cc8adc6e38580468927278f21e89a1f4d
Samuel Morakinyo <s.morakinyo@corecomtechacademy.co.uk>
2023-08-16 14:06:45 +0100

    About Arooj

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('#bmwi8').click(function(){
+        alert('Close!!!');
+    });
+});
+
+$(document).ready(function(){
+    $('#Porsche 911 Turbo S').click(function(){
+        alert('That\'s it!');
+    });
+});
+
+$(document).ready(function(){
+    $('#Aston Martin DB9').click(function(){
+        alert('Try Again!!!');
+    });
+});
